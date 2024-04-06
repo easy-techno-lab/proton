0d63cff8032c7fa444123f70048895d90bae6530
Yevhen Hromiei <1124grom@gmail.com>
2024-04-06 09:57:48 +0300

    Removed utils.CopySlice function, now available as slices.Clone

--- utils/generic.go
+++ utils/generic.go
@@ -1,12 +1,6 @@
 package utils
 
-func CopySlice[T any](src []T) []T {
-	dst := make([]T, len(src))
-	copy(dst, src)
-	return dst
-}
-
-func CopyStruct[T any](src *T) *T {
+func ClonePointer[T any](src *T) *T {
 	dst := new(T)
 	*dst = *src
 	return dst
